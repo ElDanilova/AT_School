4deeab10c79f5f557c9f526469d2198e850ca86e
ElDanilova <shelena.spb@gmail.com>
2020-04-19 17:54:15 +0300

    Add files via upload

---
+++ task_3.3.py
@@ -0,0 +1,15 @@
+def season (n):
+    n = int(n)
+    if n == 1 or n == 2 or n == 12:
+        return ("зима")
+    elif n == 3 or n == 4 or n == 5:
+        return ("весна")
+    elif n == 6 or n == 7 or n == 8:
+        return ("лето")
+    else:
+        return ("осень")
+
+# Программа
+print("Добро пожаловать! Перед вами программа, определяющая время года по введённому месяцу.")
+m = input("Введите номер месяца: ")
+print("Это", season(m))
