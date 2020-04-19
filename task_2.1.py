a0109d09208fe5253e1a4ddbee711318f46218c9
ElDanilova <shelena.spb@gmail.com>
2020-04-19 14:26:28 +0300

    Домашнее задание 2.1
    Требования
    Создать консольный калькулятор работающий с положительными целыми числами.
    1. Программа по очереди запрашивает: 
           а) операнд 1
           б) операнд 2
           в) одну из 4-х операций: + - / *
    2. Проводит вычисление и выводит результат
    3. Запрашивает завершение
    4. Если завершения нет, повторяется пункт 1

---
+++ task_2.1.py
@@ -0,0 +1,60 @@
+# Вводная о программе
+print("Добро пожаловать в консольный калькулятор!\nЭта программа работает с целыми числами и умеет складывать, вычитать, делить и умножать.\n")
+
+# Запускаем бесконечный калькулятор
+while True:
+
+    # Переводим первый ввод в целое число
+    while True:
+        try:
+            num1 = int(input("Введите первый операнд - целое число: "))
+            break
+        except ValueError:
+            print("Это не целое число.")
+
+    # Переводим второй ввод в целое число
+    while True:
+        try:
+            num2 = int(input("Введите второй операнд - целое число: "))
+            break
+        except ValueError:
+            print("Это не целое число.")
+
+    # Определяем операцию над числами
+    op_dic = ("+", "-", "*", "/")
+    while True:
+        op = input("Введите совершаемую на числами операцию в виде команды: + , - , / , * \n")
+        fl = False
+        for i in op_dic:
+            if op == i:
+                fl = True
+                break
+        if not fl:
+            print("Вы ввели неизвестную команду.")
+        else:
+            break
+
+    # Производим вычисления
+    fl2 = True
+    calc = 0
+    if op == "+":
+        calc = num1 + num2
+    elif op == "-":
+        calc = num1 - num2
+    elif op == "*":
+        calc = num1 * num2
+    elif num2 != 0:
+        calc = num1 / num2
+    else:
+        fl2 = False
+        print("Деление на ноль невозможно!")
+
+    # Вывод результата
+    if fl2:
+        print("Результат операции", num1, op, num2,"=",calc)
+
+    # Продолжение цикла
+    num_or_stop = input("Для продолжения вычеслений нажмите Enter. \nДля завершения вычислений введите команду 'stop'.\n")
+    if num_or_stop == "stop" or num_or_stop == "Stop" or num_or_stop == "STOP":
+        print("Вы вышли из калькулятора.")
+        break
